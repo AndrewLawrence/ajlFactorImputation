9ed1da8ee7167d3a1d62940b2e4b287b4f78dfe8
Andrew Lawrence <k1641319@kcl.ac.uk>
2025-05-22 14:24:35 +0100

    [example] simplify readme example

--- inst/examples/factor_imputation_readme.R
+++ inst/examples/factor_imputation_readme.R
@@ -1,18 +1,18 @@
 library(ajlFactorImputation)
 
-# View help for main function
+# View help for the main function:
 ?factor_imputation
 
-# load raw five factor personality data
-#   (example of a factor structure with missing values)
+# For this example, we work with personality data from the psych package
+#   (an example of a 5-factor structure with missing values)
 data("bfi", package = "psych")
 
-# convert gender to a factor:
+# Convert gender to a factor:
 bfi$gender <- factor(bfi$gender, levels = 1:2, labels = c("Male", "Female"))
 
-# run factor imputation requesting a 5-factor solution
-#   (5 factors known structure for this data), using package default
-#   PAF factor analysis options
+# Run a factor imputation requesting a 5-factor solution
+#   (5 factors known structure for this data), using the package defaults
+#   for factor analysis (rather than pca).
 #
 # [ ! Note: this step is time-consuming ! ]
 bfi_5f_result <- ajlFactorImputation::factor_imputation(
@@ -21,14 +21,26 @@ bfi_5f_result <- ajlFactorImputation::factor_imputation(
   options = fami_options(fa_type = "fa", nfactors = 5L)
 )
 
+# Printing the result gives some basic information:
+bfi_5f_result
+
+# The package includes accessor functions to extract:
+#   1. the data covariance matrix (pooled over imputations)
+get_pooledcov(bfi_5f_result)
+#   2. the factor loadings
+get_loadings(bfi_5f_result)
+#   3. the multiply imputed data set (mids)
+#         (this includes factor scores for each imputation)
+get_mids(bfi_5f_result)
+
 # inspect the factor structure:
 print(get_loadings(bfi_5f_result), cutoff = 0.35)
 
-# extract a mids object to use in further analysis:
+# extract the mids object to use in further analysis:
 bfi_5f_mids <- get_mids(bfi_5f_result)
 
 # fit a simple regression model to the data using imputed factor scores.
-#   Specifically, test the hypothesis that factor PA1
+#   Specifically we will test the hypothesis that factor PA1
 #   (which corresponds to Extraversion) does not differ between genders.
 library(mice)
 mi_models <- with(bfi_5f_mids, lm(PA1 ~ gender))
@@ -38,47 +50,23 @@ mi_pooled <- mice::pool(mi_models)
 # The pooled analysis result:
 summary(mi_pooled)
 #           term    estimate  std.error statistic       df     p.value
-# 1  (Intercept)  0.07331121 0.03297476  2.223252 2783.833 0.026278810
-# 2 genderFemale -0.10969545 0.04024731 -2.725535 2777.805 0.006459998
-
+# 1  (Intercept)  0.07378399 0.03295151  2.239169 2788.247 0.025223489
+# 2 genderFemale -0.11008271 0.04019658 -2.738609 2790.070 0.006209165
 
 # Great, we can reject the null hypothesis of no difference.
 
-# Note that above we specified the number of factors according to
-#   theory (the "big5" personality structure). If instead we didn't
-#   know the ideal number of factors for the data we could have
-#   the package estimate this.
-#   Estimation by parallel analysis is the default behaviour of
-#   the factor_imputation function.
+# We can compare to a complete-cases model:
+cc_model <- lm(PA1 ~ gender, data = complete(bfi_5f_mids, 0L))
+# There are only 2436 observations (rather than 2800)
 
-# We can see what would happen by rerunning factor_imputation, but
-#   without nfactors set, or with fami_options(nfactors = "parallel").
+broom::tidy(cc_model)
+# # A tibble: 2 x 5
+#   term         estimate std.error statistic p.value
+#   <chr>           <dbl>     <dbl>     <dbl>   <dbl>
+# 1 (Intercept)    0.0971    0.0354      2.74 0.00619
+# 2 genderFemale  -0.118     0.0433     -2.73 0.00645
 
-# However, this would re-impute the data.
-#   In this case it will be quicker just to check if this *would* have selected
-#   the appropriate number of factors by applying an equivalent parallel
-#   analysis to the pooled multiply-imputed covariance matrix
-#   (which has already been calculated).
-get_pooledcov(bfi_5f_result)
-
-set.seed(42)
-psych::fa.parallel(get_pooledcov(bfi_5f_result),
-                   n.obs = nrow(bfi),
-                   fm = "pa", fa = "fa",
-                   n.iter = 100,
-                   sim = TRUE,
-                   error.bars = TRUE,
-                   quant = 0.99,
-                   se.bars = TRUE)
-
-# Close. It identified 6.
-
-# Velicer's Minimum Average Partial method identifies 5 though
-which.min(psych::nfactors(
-  get_pooledcov(bfi_5f_result),
-  n = 10,
-  n.obs = nrow(bfi),
-  fm = "pa",
-  rotate = "oblimin"
-)$map)
-# [1] 5
+# In this example the results are very similar.
+#   Coefficient standard errors are reduced by multiple imputation,
+#   but the effect estimate for gender is also marginally smaller, so the
+#   key t-statistics are comparable.
